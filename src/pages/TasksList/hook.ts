aae0e1702a0527b6ff81edf3f6c41a1d4a1606ba
Leandro Aranguez <leandro.aranguez@globallogic.com>
2022-04-07 13:39:31 -0300

    docs(TaskList): agrega comentarios a funciones

--- src/pages/TasksList/hook.ts
+++ src/pages/TasksList/hook.ts
@@ -3,20 +3,38 @@ import { useState } from "react";
 import { Itask } from "../../types/task.model";
 import { v4 as uuidv4 } from "uuid";
 
+/**
+ * @description taskList custom hook
+ * @author Leandro Aranguez
+ */
 const useTaskList = () => {
   const [tasks, setTasks] = useState<Itask[]>([]);
   const [value, setValue] = useState("");
 
+  /**
+   * @description add task to state.
+   * @author Leandro Aranguez
+   */
   const addTask = () => {
     const id = uuidv4();
     setTasks([...tasks, { id, text: value }]);
     setValue("");
   };
 
+  /**
+   * @description edit task
+   * @param task
+   * @author Leandro Aranguez
+   */
   const editTask = (task: Itask) => {
     console.log("edit", task);
   };
 
+  /**
+   * @description delete task
+   * @param task
+   * @author Leandro Aranguez
+   */
   const deleteTask = (task: Itask) => {
     console.log("delete", task);
   };
